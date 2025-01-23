640ffd1f6269e5cd039b2a27c9ca216f9a8ffaa3
Ogbonna Blessed <blessedlyrics11@gmail.com>
2025-01-23 12:00:00 +0100

    update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,19 @@
-import express from "express"
-import dotenv from "dotenv"
-import bodyParser from "body-parser"
-import cors from "cors"
-import helmet from "helmet"
-import morgan from "morgan"
-import * as dynamoose from "dynamoose"
-import serverless from "serverless-http"
-import { clerkMiddleware, createClerkClient, requireAuth } from "@clerk/express"
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import * as dynamoose from "dynamoose";
+import serverless from "serverless-http";
+import { clerkMiddleware, createClerkClient, requireAuth } from "@clerk/express";
 
 /* Route Imports */
-import courseRoutes from './routes/courseRoutes'
-import userClerkRoutes from './routes/userClerkRoutes'
-import transactionRoutes from './routes/transactionRoutes'
-import userCourseProgressRoutes from './routes/userCourseProgressRoutes'
-import seed from "./seed/seedDynamodb"
+import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
+import transactionRoutes from "./routes/transactionRoutes";
+import userCourseProgressRoutes from "./routes/userCourseProgressRoutes";
+import seed from "./seed/seedDynamodb";
 
 /* Configurations */
 dotenv.config();
@@ -21,11 +21,11 @@ dotenv.config();
 const isProduction = process.env.NODE_ENV === "production";
 
 if (!isProduction) {
-    dynamoose.aws.ddb.local();
+  dynamoose.aws.ddb.local();
 }
 
 export const clerkClient = createClerkClient({
-    secretKey: process.env.CLERK_SECRET_KEY,
+  secretKey: process.env.CLERK_SECRET_KEY,
 });
 
 const app = express();
@@ -40,7 +40,7 @@ app.use(clerkMiddleware());
 
 /* Routes */
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.use("/courses", courseRoutes);
@@ -49,23 +49,23 @@ app.use("/transactions", requireAuth(), transactionRoutes);
 app.use("/users/course-progress", requireAuth(), userCourseProgressRoutes);
 
 /* Server */
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT || "8001", 10);
 if (!isProduction) {
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 }
 
 // aws production environment
 const serverlessApp = serverless(app);
 export const handler = async (event: any, context: any) => {
-    if (event.action === "seed") {
-        await seed();
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "Data seeded successfully" }),
-        };
-    } else {
-        return serverlessApp(event, context);
-    }
+  if (event.action === "seed") {
+    await seed();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Data seeded successfully" }),
+    };
+  } else {
+    return serverlessApp(event, context);
+  }
 };
